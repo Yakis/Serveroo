dff6a65bc3232a2b4c55deabef1e0424a0af31bd
Mugurel Moscaliuc <yakis@Mugurels-iMac.local>
2018-05-29 14:21:43 +0100

    Update and delete methods working

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -16,10 +16,20 @@ final class UsersController: RouteCollection {
     func boot(router: Router) throws {
         let users = router.grouped("users")
         
-        users.post(User.self, use: create)
-        users.get(use: getAll)
+        // /api/v1/users/create
+        users.post("create", use: create)
+        
+        // /api/v1/users/all
+        users.get("all", use: getAll)
+        
+        // /api/v1/users/:id
         users.get(User.parameter, use: getOne)
+        users.delete(User.parameter, use: delete)
+        users.patch(User.parameter, use: update)
+        
+        // /api/v1/users?username=:username
         users.get(use: searchByName)
+        
     }
     
     
@@ -33,7 +43,7 @@ final class UsersController: RouteCollection {
     }
     
     
-    func create(_ req: Request, _ user: User) throws -> Future<User> {
+    func create(_ req: Request) throws -> Future<User> {
         return try req.content.decode(User.self).flatMap { user in
             return user.save(on: req)
         }
@@ -42,9 +52,8 @@ final class UsersController: RouteCollection {
     
     func searchByName(_ req: Request) throws -> Future<User> {
         let username: String = try req.query.get(at: "username")
-        print("You're looking for a user named=== \(username)")
         return req.withNewConnection(to: .psql) { db -> Future<User> in
-            return try db.query(User.self).filter(\User.username ~~ username).first().map(to: User.self) { user in
+            return try db.query(User.self).filter(\.username ~~ username).first().map(to: User.self) { user in
                 guard let user = user else {
                     throw Abort(.notFound, reason: "Could not find user.")
                 }
@@ -53,6 +62,33 @@ final class UsersController: RouteCollection {
             }
         }
     }
+    
+    
+    func update(_ req: Request) throws -> Future<User> {
+        return try req.parameters.next(User.self).flatMap { user in
+            return try req.content.decode(UserContent.self).flatMap { newUser in
+                user.username = newUser.username ?? user.username
+                user.email = newUser.email ?? user.email
+                user.firstName = newUser.firstName ?? user.firstName
+                user.lastName = newUser.lastName ?? user.lastName
+                user.contactNumber = newUser.contactNumber ?? user.contactNumber
+                user.postcode = newUser.postcode ?? user.postcode
+                user.avatar = newUser.avatar ?? user.avatar
+                user.deviceToken = newUser.deviceToken ?? user.deviceToken
+                user.firebaseUid = newUser.firebaseUid ?? user.firebaseUid
+                user.userType = newUser.userType ?? user.userType
+                return user.save(on: req)
+            }
+        }
+    }
+    
+    
+    func delete(_ req: Request) throws -> Future<HTTPStatus> {
+        return User.query(on: req).all().flatMap(to: HTTPStatus.self) { users in
+            return users[0].delete(on: req).transform(to: HTTPStatus.noContent)
+        }
+    }
+    
     
     
 }
